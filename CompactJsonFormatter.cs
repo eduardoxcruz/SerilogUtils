67bf43e7dee4a239889d760b5723a1eab603fd0c
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-03-02 17:03:30 -0600

    Add CompactJsonFormatter class

---
+++ CompactJsonFormatter.cs
@@ -0,0 +1,11 @@
+﻿using Serilog.Formatting;
+
+namespace DefaultNamespace;
+
+/// <summary>
+/// An <see cref="ITextFormatter"/> that writes events in a compact JSON format.
+/// </summary>
+public class CompactJsonFormatter
+{
+    
+}
