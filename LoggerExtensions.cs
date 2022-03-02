8509793dfea57b088a3ee5a16f5f1806d053a0bf
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-03-02 17:07:13 -0600

    Add Here() method

--- LoggerExtensions.cs
+++ LoggerExtensions.cs
@@ -1,6 +1,17 @@
-﻿namespace SerilogUtils;
+﻿using System.Runtime.CompilerServices;
+using Serilog;
+
+namespace SerilogUtils;
 
 public static class LoggerExtensions
 {
-    
+    public static ILogger Here(this ILogger logger, 
+        [CallerMemberName] string memberName = "",
+        [CallerFilePath] string sourceFilePath = "", 
+        [CallerLineNumber] int sourceLineNumber = 0)
+    {
+        return logger.ForContext("MemberName", memberName)
+            .ForContext("FilePath", sourceFilePath)
+            .ForContext("LineNumber", sourceLineNumber);
+    }
 }
